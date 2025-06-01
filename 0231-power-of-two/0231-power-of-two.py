b001f57d554d5da51ac90a84ebb87a015bc9d184
VKrishna04 <75069043+VKrishna04@users.noreply.github.com>
2025-06-01 23:31:29 +0530

    Time: 0 ms (100%), Space: 17.7 MB (55.48%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,12 +1,14 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        n = str(bin(n))[2:]
-        print(n)
+        # n = str(bin(n))[2:]
+        # print(n)
     
-        if n[0] != '1':
-            return False
+        # if n[0] != '1':
+        #     return False
         
-        for i in range(1,len(n)):
-            if n[i] != '0':
-                return False
-        return True
+        # for i in range(1,len(n)):
+        #     if n[i] != '0':
+        #         return False
+        # return True
+
+        return n > 0 and str(bin(n)).count('1') == 1
